a7810308672ddb2697d74cebf07e7952d3cf3220
Philip lambok <philiplambok71@gmail.com>
2018-07-14 20:15:12 +0700

    add forbidden features

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,28 +1,47 @@
 class Api::BlogsController < ApplicationController
   before_action :find_all_blogs, only: [:index]
-  before_action :set_blog, only: [:show, :user]
-  before_action :validate_availability_user, only: [:show, :user]
-  before_action :must_signed_in, only: [:create]
+  before_action :set_blog, only: [:show, :user, :update, :destroy]
+  before_action :validate_availability_blog, only: [:show, :user, :update, :destroy]
+  before_action :must_signed_in, only: [:create, :update, :destroy]
+  before_action :must_same_user, only: [:update, :destroy]
 
+  # listing blogs 
   def index 
     json_response(@blogs) 
   end
 
+  # show data blog
   def show 
     json_response(@blog)
   end
 
+  # show user corresponding to blog
   def user
     json_response(@blog.user)
   end
 
+  # add new blog
   def create
-    @blog = current_user.blogs.build blog_params 
+    @blog = current_user.blogs.build(blog_params)
     
     if @blog.save 
       json_response(@blog)
     end
   end
+
+  # update attribute blog
+  def update 
+    if @blog.update_attributes(blog_params)
+      json_response(@blog)
+    end
+  end
+
+  # delete blog
+  def destroy 
+    if @blog.destroy 
+      json_response(@blog)
+    end
+  end 
 
   private
   def blog_params
@@ -37,11 +56,23 @@ class Api::BlogsController < ApplicationController
     @blog = Blog.find_by_id(params[:id])
   end
 
-  def validate_availability_user 
+  def validate_availability_blog 
     error_response(code: 404, message: "Link not found") unless @blog
   end
 
   def must_signed_in
-    error_response code: 401, message: "Sorry, you must signed in." unless authenticated? 
+    non_authenticated_message unless authenticated? 
+  end
+
+  def non_authenticated_message 
+    error_response code: 401, message: "Sorry, you must signed in."
+  end
+
+  def forbidden_message
+    error_response code: 403, message: "Sorry, you dont have permission to do this." 
+  end
+
+  def must_same_user 
+    forbidden_message unless current_user == @blog.user
   end
 end
